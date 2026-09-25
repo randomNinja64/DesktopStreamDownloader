bc613a2ae155a8755121c05391c955ef2c6dfd11
randomNinja64 <43556413+randomNinja64@users.noreply.github.com>
2026-09-25 12:28:10 -0500

    Remove Dead Code

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Net;
 using System.Windows.Forms;
 
 namespace DesktopStreamDownloader
@@ -12,19 +11,6 @@ namespace DesktopStreamDownloader
         [STAThread]
         static void Main()
         {
-            try
-            {
-                // Tls11 = 768, Tls12 = 3072, Tls13 = 12288 (.NET 3.5 only names Tls/1.0)
-                ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls
-                    | (SecurityProtocolType)768
-                    | (SecurityProtocolType)3072
-                    | (SecurityProtocolType)12288;
-            }
-            catch
-            {
-                // Keep system defaults on hosts that reject newer protocols.
-            }
-
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
